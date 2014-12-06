1543c35fca4aded96312190027d21fb9d0a80541
tommaes <tommaes7@hotmail.com>
2014-12-06 13:10:49 +0000

    Add message for login.

--- src/server/database/user_info.js
+++ src/server/database/user_info.js
@@ -1,7 +1,19 @@
+var answer = require('../../general/loginAnswer')
+
 module.exports = {
 
     User_Info_DB : function () {
     
+        function login(username, password, db) {
+            db.get("SELECT * from user_info where username=? AND password=?", [username, password] , function(err,row){
+               if(row === undefined) {
+                   return new LogInAnswer(false, undefined);
+               }
+               else {
+                   return new LogInAnswer(true, undefined);
+               }
+            });
+        }
         
 
     
