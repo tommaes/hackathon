38f7a17e3cc5b6786aa74b5ae675383d09a9fd30
tommaes <tommaes7@hotmail.com>
2014-12-06 10:38:40 +0000

    second commit

--- src/server/database/Database.js
+++ src/server/database/Database.js
@@ -5,18 +5,24 @@ var db = new sqlite3.Database('heroesofmightandknowledge.db');
 var check;
 db.serialize(function() {
 
-  db.run("CREATE TABLE if not exists user_info (info TEXT)");
-  var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+  db.run("CREATE TABLE if not exists user_info (username TEXT NOT NULL, \
+                                                password TEXT NOT NULL, \
+                                                email TEXT NOT NULL, \
+                                                hero TEXT NOT NULL, \
+                                                typeaccount INTEGER NOT NULL, \
+                                                PRIMARY KEY(username))");
+  //db.run("CREATE TABLE if not exists ")
+  var stmt = db.prepare("INSERT INTO user_info VALUES (?, ?, ?, ?, ?)");
   for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
+      stmt.run("test" + i, "Ipsum " + i, "lol", "lal", 1);
   }
   stmt.finalize();
 
-  db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
-      console.log(row.id + ": " + row.info);
+  db.each("SELECT rowid AS id, username FROM user_info", function(err, row) {
+      console.log(row.id + ": " + row.username);
   });
   
-  db.run("DROP TABLE user_info")
+  db.run("DROP TABLE user_info");
 });
 
 db.close();
